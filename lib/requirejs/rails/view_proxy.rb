9943662da22329f07a51eb8258706b69a7411d02
Strech (Sergey Fedorov) <oni.strech@gmail.com>
2016-04-05 18:42:19 +0500

    ActionView::Base is more complicated now

--- lib/requirejs/rails/view_proxy.rb
+++ lib/requirejs/rails/view_proxy.rb
@@ -1,14 +1,5 @@
 module Requirejs
   module Rails
-    class ViewProxy
-      include ActionView::Context
-
-      if ::Rails::VERSION::MAJOR >= 4
-        include ActionView::Helpers::AssetUrlHelper
-        include ActionView::Helpers::TagHelper
-      else
-        include ActionView::Helpers::AssetTagHelper
-      end
-    end
+    ViewProxy = Class.new(ActionView::Base)
   end
 end
